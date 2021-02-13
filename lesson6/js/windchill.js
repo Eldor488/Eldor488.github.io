f72122d245d799489e1fc0bb239c851df8ae584a
Eldor488 <del20047@byui.com>
2021-02-12 21:38:16 -0400

    master

---
+++ lesson6/js/windchill.js
@@ -0,0 +1,26 @@
+ /* Input: get temperature and wind speed from the user
+       * Processing: calculate the wind chill factor
+       * Output:  display wind chill factor to the user
+       */
+      function doInputOutput() {
+        let temperature = 50;
+        let windSpeed = 3;
+        
+        let windChillVariable = windChill(temperature, windSpeed);
+        document.getElementById("output").innerHTML = windChillVariable.toFixed(
+          3
+        );
+      }
+      // Return the wind chill factor in Fahrenheit
+      //  formula for the wind chill factor
+
+      function windChill(temperature, windSpeed) {
+        let windChillVariable =
+          35.74 +
+          0.6215 * temperature -
+          35.75 * windSpeed ** 0.16 +
+          0.4275 * temperature * windSpeed ** 0.16;
+        return windChillVariable;
+      }
+
+      doInputOutput();
