c4a46e0a61695e09d686a1b5f2578739486c6ca9
Eldor488 <del20047@byui.com>
2021-02-18 00:34:43 -0400

    finished babe

--- lesson7/lazy.js
+++ lesson7/lazy.js
@@ -6,4 +6,22 @@ const loadImages = (image) => {
   image.onload = () => {
     image.removeAttribute('data-src');
   };
-};
+}; 
+
+if('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if(item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  });
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
