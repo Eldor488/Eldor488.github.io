bc705095c90506b9b42cf919a617a1f64ce9c828
Eldor488 <del20047@byui.com>
2021-02-26 08:56:00 -0400

    loli

---
+++ lesson8/js/script.js
@@ -0,0 +1,45 @@
+function toggleMenu() { 
+ document.getElementById("list-items").classList.toggle("hide"); 
+}
+
+
+function pancakeBanner() {
+    let d = new Date();
+    let n = d.getDay(); 
+    
+    if (n == 5) {
+        document.getElementById("pancakes-saturday").style.display = "block";
+    } else {
+        document.getElementById("pancakes-saturday").style.display = "none";
+    } 
+}
+
+ pancakeBanner(); 
+
+ let imagesToLoad = document.querySelectorAll('img[data-src]'); 
+
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src')); 
+
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+}; 
+
+if('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver((items, observer) => {
+    items.forEach((item) => {
+      if(item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  });
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
